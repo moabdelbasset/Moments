c939b491e8d28feaaaf3cb5c2742ede888caf242
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-01-02 15:03:47 +0000

    adding custom CSS

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/logo.png'
+import styles from '../styles/NavBar.module.css'
 
 export const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top"><Container>
+    <Navbar className={styles.NavBar} expand="md" fixed="top"><Container>
   <Navbar.Brand>
     <img src={logo} alt="logo" height="45" />
   </Navbar.Brand>
