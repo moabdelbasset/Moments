f928b3a946429c20316814efb443b75cd1d69c42
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2024-01-16 19:04:10 +0000

    Create comment form

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,11 +8,15 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
-
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({results: []});
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -38,7 +42,17 @@ function PostPage() {
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <p>Post component</p>
         <Container className={appStyles.Content}>
-          Comments
+            {currentUser ? (
+            <CommentCreateForm
+            profile_id={currentUser.profile_id}
+            profileImage={profile_image}
+            post={id}
+            setPost={setPost}
+            setComments={setComments}
+          />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
